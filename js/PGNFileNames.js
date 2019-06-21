df322c8de6cf615782cd6e8260fa00ff64fc3cb5
unknown <someuseremail@someuserexample.com>
2019-06-21 16:17:48 +0200

    Book

--- js/PGNFileNames.js
+++ js/PGNFileNames.js
@@ -7,5 +7,6 @@ function GetWhiteFileNameList(){
 
 function GetBlackFileNameList(){
     return ['games/lichess_study_black-danish-gambit_by_BlackSalt_2019.06.12.pgn', 
-    'games/lichess_study_kings-gambit_by_BlackSalt_2019.06.14.pgn']
+    'games/lichess_study_kings-gambit_by_BlackSalt_2019.06.14.pgn',
+'games/lichess_study_indian-game_by_BlackSalt_2019.06.21.pgn']
 }
