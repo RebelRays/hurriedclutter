ff516524aea31bc693de20259f87b5824a5df615
unknown <someuseremail@someuserexample.com>
2019-12-22 02:02:19 +0100

    pick study

--- js/PGNFileNames.js
+++ js/PGNFileNames.js
@@ -1,20 +1,12 @@
 
 function GetWhiteFileNameList(){
-    return ['games/fchess.black.pgn'];
-}
-
-function GetBlackFileNameList(){
-    return ['games/fchess.white.pgn'];
-}
-
-function GetWhiteFileNameList2(){
-    return ['games/lichess_study_myopeninglibrary_by_BlackSalt_2019.06.16.pgn', 
+    return ['games/lichess_study_minecraftado-black_by_BlackSalt_2019.12.14.pgn','games/fchess.black.pgn','games/lichess_study_myopeninglibrary_by_BlackSalt_2019.06.16.pgn', 
     'games/lichess_study_caro-can_by_BlackSalt_2019.06.14.pgn',
     'games/lichess_study_counter-scandi_by_BlackSalt_2019.06.19.pgn', 'games/lichess_study_whiteopeningbook_by_BlackSalt_2019.10.07.pgn'];
 }
 
-function GetBlackFileNameList2(){
-    return ['games/lichess_study_black-danish-gambit_by_BlackSalt_2019.06.12.pgn', 
+function GetBlackFileNameList(){
+    return ['games/fchess.white.pgn', 'games/lichess_study_black-danish-gambit_by_BlackSalt_2019.06.12.pgn', 
     'games/lichess_study_kings-gambit_by_BlackSalt_2019.06.14.pgn',
 'games/lichess_study_indian-game_by_BlackSalt_2019.06.21.pgn'];
 }
