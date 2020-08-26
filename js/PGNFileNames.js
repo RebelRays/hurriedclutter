ef8fc5fcec69eb54775d8961558e7a8c28fd24a0
unknown <someuseremail@someuserexample.com>
2020-08-26 02:23:48 +0200

    d4

--- js/PGNFileNames.js
+++ js/PGNFileNames.js
@@ -3,7 +3,8 @@ function GetWhiteFileNameList(){
     return ['games/lichess_study_minecraftado-black_by_BlackSalt_2019.12.14.pgn','games/fchess.black.pgn','games/lichess_study_myopeninglibrary_by_BlackSalt_2019.06.16.pgn', 
     'games/lichess_study_caro-can_by_BlackSalt_2019.06.14.pgn',
     'games/lichess_study_barnes_by_BlackSalt_2019.12.30.pgn',
-    'games/lichess_study_counter-scandi_by_BlackSalt_2019.06.19.pgn', 'games/lichess_study_whiteopeningbook_by_BlackSalt_2019.10.07.pgn'];
+    'games/lichess_study_counter-scandi_by_BlackSalt_2019.06.19.pgn', 'games/lichess_study_whiteopeningbook_by_BlackSalt_2019.10.07.pgn',
+'games/d4_qgd-three-knights-be7-d5-exchange.pgn'];
 }
 
 function GetBlackFileNameList(){
@@ -12,5 +13,5 @@ function GetBlackFileNameList(){
 'games/lichess_study_indian-game_by_BlackSalt_2019.06.21.pgn',
 'games/nf6-nuttyvariation.pgn',
 'games/Nf6_Trident.pgn', 'games/Nf6_scandi.pgn', 'games/Nf6_4nf3-take-my-pawns.pgn', 'games/Nf6_Knighthugs.pgn', 'games/Nf6_Knightsqueeze.pgn'
-,'games/Nf6_bc4-d3-pawndrop.pgn'];
+,'games/Nf6_bc4-d3-pawndrop.pgn', 'games/Nf6_4-pawn-Nc3.pgn'];
 }
